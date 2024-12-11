1b99ce1c800a5b1faa254929b659e6d3609474bc
vadlakondaswetha <swethv@google.com>
2024-12-11 12:27:25 +0000

    PR review comments

--- internal/fs/inode/file_test.go
+++ internal/fs/inode/file_test.go
@@ -743,7 +743,7 @@ func (t *FileTest) TestTruncateUpwardForEmptyGCSFileWhenStreamingWritesAreEnable
 		},
 		{
 			name:         "WithOutWrite",
-			performWrite: true,
+			performWrite: false,
 		},
 	}
 	for _, tc := range tbl {
